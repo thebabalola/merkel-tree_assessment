bd33478eff829b2a7f26405b6fde8de9a57303c1
thebabalola <t.babalolajoseph@gmail.com>
2025-01-25 20:04:44 +0100

    address to be verified change, to test if address pass if a wrong address is inputed

--- merkle.js
+++ merkle.js
@@ -2,26 +2,22 @@ const { MerkleTree } = require('merkletreejs');
 const SHA256 = require('crypto-js/sha256');
 
 const addresses = [
-  "0x123456789abcdef123456789abcdef123456789a",
+    "0xabcdefabcdefabcdefabcdefabcdefabcdefabcde",
   "0xabcdef123456789abcdef123456789abcdef1234",
   "0x789abcdef123456789abcdef123456789abcdef12",
-  "0x456789abcdef123456789abcdef123456789abcdef",
-  "0xabcdefabcdefabcdefabcdefabcdefabcdefabcde"
+  "0x123456789abcdef123456789abcdef123456789a",
+  "0x456789abcdef123456789abcdef123456789abcdef"
 ];
-
 
 const leafNodes = addresses.map(addr => SHA256(addr).toString());
 const merkleTree = new MerkleTree(leafNodes, SHA256); //Hash addresses
 
-
 const root = merkleTree.getRoot().toString('hex');
-console.log("Merkle Root:", root); // Get merkle root
-
+console.log("The Merkle-Root:", root); // Get merkle root
 
 const addressToVerify = "0x789abcdef123456789abcdef123456789abcdef12";
 const leafToVerify = SHA256(addressToVerify).toString();
 const proof = merkleTree.getProof(leafToVerify); // Generates proof for specific address
-
 
 const isValid = merkleTree.verify(proof, leafToVerify, root);
 console.log(`This Address ${addressToVerify} is part of the tree?`, isValid); // Verify's the proof, if it exist or not
