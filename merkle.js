1630c7b58688df2893387d0ec29c6621ee3c6095
thebabalola <t.babalolajoseph@gmail.com>
2025-01-25 20:06:56 +0100

    address verified

--- merkle.js
+++ merkle.js
@@ -2,7 +2,7 @@ const { MerkleTree } = require('merkletreejs');
 const SHA256 = require('crypto-js/sha256');
 
 const addresses = [
-    "0xabcdefabcdefabcdefabcdefabcdefabcdefabcde",
+  "0xabcdefabcdefabcdefabcdefabcdefabcdefabcde",
   "0xabcdef123456789abcdef123456789abcdef1234",
   "0x789abcdef123456789abcdef123456789abcdef12",
   "0x123456789abcdef123456789abcdef123456789a",
@@ -20,5 +20,5 @@ const leafToVerify = SHA256(addressToVerify).toString();
 const proof = merkleTree.getProof(leafToVerify); // Generates proof for specific address
 
 const isValid = merkleTree.verify(proof, leafToVerify, root);
-console.log(`This Address ${addressToVerify} is part of the tree?`, isValid); // Verify's the proof, if it exist or not
+console.log(`Is this Address ${addressToVerify} a member of the tree?`, isValid); // Verify's the proof, if it exist or not
 
